49e7570f0ea87c7b766b24731ed4c0136c4de81e
Federico <federicobiaus@gmail.com>
2026-06-03 14:18:18 -0300

    fix update user

--- backend/src/users/dto/update-user-dto.ts
+++ backend/src/users/dto/update-user-dto.ts
@@ -1,9 +1,9 @@
+import { IsOptional, IsString } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
-
 import { CreateUserDto } from './create-user-dto';
 
-export class UpdateUserDto extends PartialType(
-  CreateUserDto,
-) {
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsString()
   phone?: string;
 }
